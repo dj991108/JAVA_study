703daef71795ad801f365c808a10ed4acd4f518e
DongJun Kim <dj991108@naver.com>
2022-08-12 01:32:58 +0900

    add 2022/08/12

--- src/HashMap_pr/Input.java
+++ src/HashMap_pr/Input.java
@@ -7,7 +7,7 @@ public class Input {
 	static Scanner s = new Scanner(System.in);
 	
 	public Input() {
-	}
+	}  
 	static void set_H (HashMap<String, Integer> country) {
 		System.out.println("나라 이름과 인구를 입력하세요.(예: Korea 5000)");
 		while(true) {			
